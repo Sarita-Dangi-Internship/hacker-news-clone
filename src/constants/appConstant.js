2eacacad8beb7759242bb34febec0c731111e37e
Sarita <dsarritta@gmail.com>
2021-06-09 12:26:56 +0545

    add constants: add Base API URL constants and routes constants

---
+++ src/constants/appConstant.js
@@ -0,0 +1,5 @@
+export default {
+  BASE_API_URL: "https://hacker-news.firebaseio.com/v0",
+  BASE_PAGE_URL: "https://news.ycombinator.com",
+};
+
---
+++ src/constants/routes.js
@@ -0,0 +1,7 @@
+export const routes = {
+    index: '/',
+    topStories: '/topStories',
+    bestStories: '/bestStories',
+    newStories: '/newStories',
+    comments: '/comments',
+}
