5469144d66596f193d2a52f3269c25da2ddde542
Sarita <dsarritta@gmail.com>
2021-06-09 17:37:16 +0545

    complete fetching data: fetch and display API data for top, best,new and comments

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
-import "./styles/main.scss";
 import React, { Component } from "react";
-import Home from "./pages/Home/index";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+import { routes } from "./constants/routes";
+import "./styles/main.scss";
+import TopStories from "./pages/topStories/index";
+import BestStories from "./pages/bestStories/index";
+import NewStories from "./pages/newStories/index";
+import Comments from "./pages/comments/index";
 import NavBar from "./components/NavBar/index";
-import { BrowserRouter } from "react-router-dom";
 
 export default class App extends Component {
   render() {
     return (
       <BrowserRouter>
-      <div className="App">
-        <div className="main-container">
-        <NavBar />
-        <Home />
+        <div className="App">
+          <div className="main-container">
+            <NavBar />
+            <Switch>
+              <Route
+                path={routes.index}
+                render={() => <Redirect to={routes.topStories} />}
+                exact={true}
+              />
+              <Route path={routes.topStories} component={TopStories} />
+              <Route path={routes.bestStories} component={BestStories} />
+              <Route path={routes.newStories} component={NewStories} />
+              <Route path={routes.comments} component={Comments} exact={true} />
+              <Route path={`${routes.comments}/:id`} component={Comments} />
+            </Switch>
+          </div>
         </div>
-      </div>
       </BrowserRouter>
     );
   }
--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,24 +1,25 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import { routes } from '../../constants/routes';
 
 export default class index extends Component {
   render() {
     return (
       <nav className="nav-bar">
         <ul className="nav-bar__list">
-          <Link className="nav-bar__list-link">
+          <Link className="nav-bar__list-link" to={routes.index}>
             <li className="nav-bar__list-item nav-bar--title">Hacker News</li>
           </Link>
-          <Link className="nav-bar__list-link">
+          <Link className="nav-bar__list-link" to={routes.topStories}>
             <li>Top Stories</li>
           </Link>
-          <Link className="nav-bar__list-link">
+          <Link className="nav-bar__list-link" to={routes.newStories}>
             <li>New Stories</li>
           </Link>
-          <Link className="nav-bar__list-link">
+          <Link className="nav-bar__list-link" to={routes.bestStories}>
             <li>Best Stories</li>
           </Link>
-          <Link className="nav-bar__list-link">
+          <Link className="nav-bar__list-link" to={routes.comments}>
             <li>Comments</li>
           </Link>
         </ul>
